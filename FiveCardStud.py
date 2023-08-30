428cec3cec3bf155bdea8b2c329165e30b519a2d
charbygill <77952449+charbygill@users.noreply.github.com>
2023-08-30 00:19:55 -0400

    Update FiveCardStud.py

--- FiveCardStud.py
+++ FiveCardStud.py
@@ -1,5 +1,29 @@
 #Start by making a card class
 class Card:
     def __init__(self, face, suite):
-        self.face = face
-        self.suite = suite
+        if face < 11
+            if face == 1
+                self.face = 'A'
+            else
+                self.face = face
+        if face == 11
+            self.face = 'J'
+        if face == 12
+            self.face = 'Q'
+        if face == 13
+            self.face = 'K'
+        if suite == 1
+            self.suite = 'D'
+        if suite == 2
+            self.suite = 'C'
+        if suite == 3
+            self.suite = 'H'
+        if suite == 4
+            self.suite = 'S'
+        
+
+deck = []
+for x in range (1,5)
+    for y in range(1,14)
+        deck.append(Card(y,x))
+
